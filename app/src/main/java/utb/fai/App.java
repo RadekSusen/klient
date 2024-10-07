75289bfcbfcc32fbf42df01231aa30d226e86058
Matěj Bača <m_baca@utb.cz>
2024-10-07 14:31:40 +0000

    Replace App.java

--- app/src/main/java/utb/fai/App.java
+++ app/src/main/java/utb/fai/App.java
@@ -1,11 +1,24 @@
 package utb.fai;
 
+
 public class App {
 
     public static void main(String[] args) {
-        // TODO: Implement input parameter processing
+        if (args.length < 2) {
+            System.out.println("Usage: java App <host> <port>");
+            return;
+        }
 
-        TelnetClient telnetClient = new TelnetClient("127.0.0.1", 23);
+        String host = args[0];
+        int port;
+        try {
+            port = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid port number.");
+            return;
+        }
+
+        TelnetClient telnetClient = new TelnetClient(host, port);
         telnetClient.run(); // run telnet client
     }
 }
