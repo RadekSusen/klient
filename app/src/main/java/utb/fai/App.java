ddb85702c9e43ecc93a50e145f50c031d59db1a7
RadekSusen <95688472+RadekSusen@users.noreply.github.com>
2024-12-16 21:03:39 +0100

    Update App.java

--- app/src/main/java/utb/fai/App.java
+++ app/src/main/java/utb/fai/App.java
@@ -4,8 +4,10 @@ public class App {
 
     public static void main(String[] args) {
         // TODO: Implement input parameter processing
+        String smtpServer = args[0];
+        int smtpPort = Integer.parseInt(args[1]);
 
-        TelnetClient telnetClient = new TelnetClient("127.0.0.1", 23);
+        TelnetClient telnetClient = new TelnetClient(smtpServer, smtpPort);
         telnetClient.run(); // run telnet client
     }
 }
